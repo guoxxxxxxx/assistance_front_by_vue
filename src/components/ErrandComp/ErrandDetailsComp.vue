a74cf8f922128e75a4b553a95e7a3b02df4672c4
guo_x <guo_x0315@163.com>
2022-04-10 21:37:10 +0800

    进一步完善了跑腿功能的开发2022-04-10-21:37

--- src/components/ErrandComp/ErrandDetailsComp.vue
+++ src/components/ErrandComp/ErrandDetailsComp.vue
@@ -1,28 +1,27 @@
 <template>
   <div>
     <el-descriptions title="发布人信息">
-      <el-descriptions-item label="用户名">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="手机号">18100000000</el-descriptions-item>
-      <el-descriptions-item label="学院">信息学院</el-descriptions-item>
+      <el-descriptions-item label="用户名">{{pubUser.name}}</el-descriptions-item>
+      <el-descriptions-item label="手机号">{{pubUser.phone}}</el-descriptions-item>
+      <el-descriptions-item label="学院">{{pubUser.faculty}}</el-descriptions-item>
       <el-descriptions-item label="邮箱"
-        >guo_x0315@163.com</el-descriptions-item
+        >{{pubUser.email}}</el-descriptions-item
       >
-      <el-descriptions-item label="联系地址"
-        >江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item
+      <el-descriptions-item label="所属专业"
+        >{{pubUser.grade + pubUser.major}}</el-descriptions-item
       >
     </el-descriptions>
     <el-descriptions title="内容详细信息">
-      <el-descriptions-item label="订单编号">1</el-descriptions-item>
-      <el-descriptions-item label="类别">快递代取</el-descriptions-item>
-      <el-descriptions-item label="简述">顺丰快递代取</el-descriptions-item>
-      <el-descriptions-item label="发布日期">2000-03-15</el-descriptions-item>
-      <el-descriptions-item label="截止日期">2022-03-15</el-descriptions-item>
+      <el-descriptions-item label="订单编号">{{item.eid}}</el-descriptions-item>
+      <el-descriptions-item label="类别">{{item.title}}</el-descriptions-item>
+      <el-descriptions-item label="发布日期">{{item.pubdate}}</el-descriptions-item>
+      <el-descriptions-item label="截止日期">{{item.deadline}}</el-descriptions-item>
     </el-descriptions>
     <h5 class="mt-0">详细概述</h5>
     <b-card style="width: 80%">
       <b-media>
         <p>
-          2018年一月份已经saygoodbye，二月份是浮躁的月份。自己很少写博客，但是经常看别人写的，有做笔记的，有分享技术和面试经验的。感觉写的都很好，自己也想尝试下，记录下自己的成长，坚持写博文，希望在2018年末的时候，看到这篇博文的时候，能发现自己这一年过得很有意义...
+          {{item.details}}
         </p>
         <div id="pic_out_box">
           <div
@@ -53,6 +52,8 @@
 <script>
 // 引入评论组件
 import comment from "bright-comment";
+import axios from "axios";
+import { base_url } from "@/config";
 export default {
   methods: {
     /**
@@ -64,29 +65,26 @@ export default {
   },
   data() {
     return {
-      srcList: [
-        "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
-        "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
-        "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
-        "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
-        "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
-        "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
-        "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
-        "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
-        "https://fuss10.elemecdn.com/8/27/f01c15bb73e1ef3793e64e6b7bbccjpeg.jpeg",
-        "https://fuss10.elemecdn.com/a/3f/3302e58f9a181d2509f3dc0fa68b0jpeg.jpeg",
-        "https://fuss10.elemecdn.com/1/34/19aa98b1fcb2781c4fba33d850549jpeg.jpeg",
-        "https://fuss10.elemecdn.com/0/6f/e35ff375812e6b0020b6b4e8f9583jpeg.jpeg",
-        "https://fuss10.elemecdn.com/9/bb/e27858e973f5d7d3904835f46abbdjpeg.jpeg",
-        "https://fuss10.elemecdn.com/d/e6/c4d93a3805b3ce3f323f7974e6f78jpeg.jpeg",
-        "https://fuss10.elemecdn.com/3/28/bbf893f792f03a54408b3b7a7ebf0jpeg.jpeg",
-        "https://fuss10.elemecdn.com/2/11/6535bcfb26e4c79b48ddde44f4b6fjpeg.jpeg",
-      ],
+      eid: this.$route.query.eid,
+      srcList: [],
+      item: {},
+      pubUser: {},
     };
   },
-  components:{
-      comment,
-  }
+  components: {
+    comment,
+  },
+  mounted() {
+    // 进入详细信息界面，查询详细信息
+    axios
+      .get(base_url + "/errand/queryDetailsByEid", {
+        params: { eid: this.eid },
+      })
+      .then((resp) => {
+        this.item = resp.data.object
+        this.pubUser = this.item.pubUser
+      });
+  },
 };
 </script>
 
